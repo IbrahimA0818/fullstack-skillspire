c365188c1334609ddd980b26ac355fc53199512c
IbrahimA0818 <ibrahimajebna@Ibrahims-MacBook-Air.local>
2025-07-31 17:36:16 -0700

    Add initial project files (ignoring node_modules)

---
+++ react/demoapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function HelloWorld(){
+  return <h1>HELLO WORLD REACT!!</h1>
+}
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-a2/src/component/personalinfo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function PersonalInfo(){
+    <div>
+        <h1>
+            First Name: Ibrahim
+        </h1>
+        <h1>
+            Last Name: Ajebna
+        </h1>
+        <h1>
+            Fav food: Pizza
+        </h1>
+        <h1>
+            Fav vacation destination: Dubai
+        </h1>
+
+    </div>
+}
